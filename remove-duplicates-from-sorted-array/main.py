52f31f4e56711d09a62a8393fdc110f889f8cf35
l2m2lq@gmail.com <114723704@qq.com>
2018-09-14 10:10:24 +0800

    upload.

--- remove-duplicates-from-sorted-array/main.py
+++ remove-duplicates-from-sorted-array/main.py
@@ -16,13 +16,17 @@ class Solution:
     if le == 0: return 0
     if le == 1: return 1
     start, end = 0, 1
+    tail = -1
     while start < (le - 1) and nums[start] <= nums[end]:
       if nums[end] == nums[start]:
         if end == le - 1:
           break
         temp = nums[end]
-        for i in range(end, le - 1):
+        if temp == nums[tail]:
+          break
+        for i in range(end, le + tail):
           nums[i] = nums[i+1]
+        tail -= 1
         nums[-1] = temp
       else:
         start += 1
@@ -31,9 +35,12 @@ class Solution:
 
 if __name__ == "__main__":
   so = Solution()
-  nums = [1, 2, 2, 3, 3, 4, 4]
-  # nums = [1, 1]
+  # nums = [1, 2, 2, 3, 3, 4, 4]
+  # nums = [1, 1, 1]
+  # nums = [1, 2, 2, 2]
+  # nums = [0,0,1,1,1,2,2,3,3,4]
   # nums is passed in by reference. (i.e., without making a copy)
+  nums = [i for i in range(-999, 999)]
   le = so.removeDuplicates(nums)
   # any modification to nums in your function would be known by the caller.
   # using the length returned by your function, it prints the first len elements.
