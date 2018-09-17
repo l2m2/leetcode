c354a3481bbcc882c1db58d9b00a90d0ddd99164
l2m2 <l2m2lq@gmail.com>
2018-09-17 23:10:54 +0800

    upload.

---
+++ second-highest-salary/ms.sql
@@ -0,0 +1,7 @@
+/*
+ * @File: ms.sql
+ * @Author: leon.li(l2m2lq@gmail.com)
+ * @Date: 2018-09-17 22:52:36
+ * @Last Modified By: leon.li(l2m2lq@gmail.com>)
+ * @Last Modified Time: 2018-09-17 23:10:02
+ */
