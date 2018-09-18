f593f5d4648697ffab9ac900376716151c03694c
l2m2lq@gmail.com <114723704@qq.com>
2018-09-18 11:43:30 +0800

    upload.

--- second-highest-salary/ms.sql
+++ second-highest-salary/ms.sql
@@ -5,8 +5,20 @@
  * @Last Modified By: leon.li(l2m2lq@gmail.com>)
  * @Last Modified Time: 2018-09-17 23:10:02
  */
+-- 解法1
 SELECT TOP 1 Salary AS SecondHighestSalary FROM (
   SELECT Salary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee)
   UNION ALL
   SELECT NULL
 ) A ORDER BY Salary DESC
+
+-- 解法2
+SELECT Top 1 Salary AS SecondHighestSalary FROM (
+		SELECT Salary, ROW_NUMBER() OVER (ORDER BY Salary DESC) AS Num
+		FROM Employee
+		GROUP BY Salary 	
+		UNION
+		SELECT NULL, 2
+) A 
+WHERE A.Num = 2
+ORDER BY Salary DESC
---
+++ second-highest-salary/pg.sql
@@ -0,0 +1,12 @@
+/*
+ * @File: pg.sql
+ * @Author: leon.li(l2m2lq@gmail.com)
+ * @Date: 2018-09-18 11:06:32
+ * @Last Modified By: leon.li(l2m2lq@gmail.com>)
+ * @Last Modified Time: 2018-09-18 11:42:54
+ */
+
+SELECT lag("Salary") OVER (ORDER BY "Salary" ) AS "SecondHighestSalary"
+FROM "Employee"
+ORDER BY "Salary" DESC
+LIMIT 1
