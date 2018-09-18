545842e2c0e97c3ce58934b294ce79d8246e4dd7
l2m2lq@gmail.com <114723704@qq.com>
2018-09-18 11:05:56 +0800

    upload.

--- second-highest-salary/ms.sql
+++ second-highest-salary/ms.sql
@@ -5,3 +5,8 @@
  * @Last Modified By: leon.li(l2m2lq@gmail.com>)
  * @Last Modified Time: 2018-09-17 23:10:02
  */
+SELECT TOP 1 Salary AS SecondHighestSalary FROM (
+  SELECT Salary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+  UNION ALL
+  SELECT NULL
+) A ORDER BY Salary DESC
